acadf104b6151d6d5595d2ab6d8b54ec0fa412e3
Blackman99 <1197160272@qq.com>
2023-04-24 12:22:33 +0800

    feat: auto width with outside block

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,54 +4,57 @@
 	import '@sveltejs/site-kit/styles/index.css';
 	import '@sveltejs/site-kit/styles/code.css';
 	import { updateAttr, getBlockAttrs } from '$lib/api';
-	import debounce from "$lib/utils/debounce"
+	import debounce from '$lib/utils/debounce';
 
-	const DEFAULT_REPL_STATES =  JSON.stringify([
-				{
-					name: 'App',
-					type: 'svelte',
-					source:
-						`<scr` +
-						`ipt>
+	const DEFAULT_REPL_STATES = JSON.stringify([
+		{
+			name: 'App',
+			type: 'svelte',
+			source:
+				`<scr` +
+				`ipt>
 	let name = 'world';
 </scr` +
-						`ipt>
+				`ipt>
 
 <h1>Hello {name}!</h1>`
-				}
-			])
+		}
+	]);
 
 	let repl;
-	const ATTR_KEY = 'custom-svelte-repl'
+	const ATTR_KEY = 'custom-svelte-repl';
 
-	const getBlockEl = () => window.frameElement?.parentElement?.parentElement
+	const getBlockEl = () => window.frameElement?.parentElement?.parentElement;
 
 	const getReplState = async () => {
-		const blockNodeEl = getBlockEl()
-		if(!blockNodeEl) {
-			return DEFAULT_REPL_STATES
+		const blockNodeEl = getBlockEl();
+		if (!blockNodeEl) {
+			return DEFAULT_REPL_STATES;
 		}
-		const { data } = await getBlockAttrs(blockNodeEl.dataset.nodeId)
-		return data[ATTR_KEY] || DEFAULT_REPL_STATES
-	}
+		const { data } = await getBlockAttrs(blockNodeEl.dataset.nodeId);
+		return data[ATTR_KEY] || DEFAULT_REPL_STATES;
+	};
 
 	onMount(async () => {
-		const initialReplState = await getReplState()
-		const initialComponents = JSON.parse(initialReplState)
+		const initialReplState = await getReplState();
+		const initialComponents = JSON.parse(initialReplState);
 		repl.set({
 			components: initialComponents
 		});
+
+		// Along with block width
+		frameElement.style.width = frameElement.parentElement.style.width =
+			frameElement.parentElement.parentElement.offsetWidth + 'px';
 	});
 
 	const handleReplChange = debounce((changedComponents) => {
-		const blockEl = getBlockEl()
-		if(blockEl) {
+		const blockEl = getBlockEl();
+		if (blockEl) {
 			updateAttr(blockEl.dataset.nodeId, {
 				[ATTR_KEY]: JSON.stringify(changedComponents.detail.components)
-			})
+			});
 		}
-	})
-
+	});
 </script>
 
 <main>
